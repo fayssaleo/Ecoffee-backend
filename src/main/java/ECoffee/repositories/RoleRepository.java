9f2b90311759328d180057190b4dcf80251e89a5
sanaaen <ennajisanaa@gmail.com>
2020-08-09 17:55:58 +0100

    Role

---
+++ src/main/java/ECoffee/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package ECoffee.repositories;
+
+import ECoffee.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+}
