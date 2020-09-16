d7e20928e10e21da095bf073e34dd2c45e8ee0d2
fayssaleo <fayssal.ourezzouq@gmail.com>
2020-09-16 23:22:55 +0100

    LogRe2

--- src/main/java/ECoffee/config/WebSecuriyConfig.java
+++ src/main/java/ECoffee/config/WebSecuriyConfig.java
@@ -2,7 +2,6 @@ package ECoffee.config;
 
 import ECoffee.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,11 +9,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 // configuration for security authentication
 @Configuration
 @EnableWebSecurity
